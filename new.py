1819df67099191e71e4df6ee624e4b2c7bff1b4e
h.a.c.hiko <73479477+Vitaliktop1@users.noreply.github.com>
2021-12-25 21:29:20 +0200

    Update new.py

--- new.py
+++ new.py
@@ -1,7 +1,7 @@
 key='6668b825-72f7-41cd-b74d-dc96a041b019'
 app_name="accgen5555"
 deviceid="173d099227daf164f9373e12d358d5a5e5b0e8e551e72b51298c595062b21ae37d93a79c80a0f66326"
-emaill="zo6um4e77a@xojxe.com'
+emaill="zo6um4e77a@xojxe.com"
 passwordd="Dadara2222"
 custompwd="fakeaccpwd"
 private="e3380aa8-d724-4982-aceb-a3840fb5d00d"
