c7a4a3038a3a20d89fc3062f73ecefaf62de7ddb
h.a.c.hiko <73479477+Vitaliktop1@users.noreply.github.com>
2021-12-26 23:49:46 +0200

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 key='6668b825-72f7-41cd-b74d-dc96a041b019'
 app_name="accgen5555"
 deviceid="173d099227daf164f9373e12d358d5a5e5b0e8e551e72b51298c595062b21ae37d93a79c80a0f66326"
-emaill="ailt9vmt2@oosln.com"
-passwordd="Dadara2222"
+emaill="v49hjevo@xojxe.com"
+passwordd="kotik7618349275"
 custompwd="fakeaccpwd"
-private="e3380aa8-d724-4982-aceb-a3840fb5d00d"
-chatlink="http://aminoapps.com/p/4z6pg2"
+private="1f7c1499-8d4e-48e8-bed8-7369bb6499e4"
+chatlink="http://aminoapps.com/p/j7mdq72"
 nickname="hachiko"
